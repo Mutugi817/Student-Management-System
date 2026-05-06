7d119b5e3f252cf2281e2d5a098ddb73c1cec357
Mutugi Gakenge <gakenge7@gmail.com>
2026-05-06 13:46:51 +0300

    First Commit

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,12 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
 app.use(express.json());
+
+const allowedOrigins = {
+  origin: ['https://student-management-system-liard-tau.vercel.app/']
+}
+app.use(cors(allowedOrigins));
 
 // --- MONGODB CONNECTION ---
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/student_tracker')
