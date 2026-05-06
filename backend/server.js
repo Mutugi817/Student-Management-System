e34b3d86660e72d74affd721ef6a6cd3ad765884
Mutugi Gakenge <gakenge7@gmail.com>
2026-05-06 14:24:16 +0300

    First Commit

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
-const allowedOrigins = {
-  origin: ['https://student-management-system-liard-tau.vercel.app/']
-}
-app.use(cors(allowedOrigins));
+app.use(cors(
+  {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }
+));
 
 // --- MONGODB CONNECTION ---
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/student_tracker')
